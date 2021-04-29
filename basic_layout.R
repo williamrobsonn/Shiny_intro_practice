90797c310e192de08fd7fe9c9f8d1623491452b0
Will Robson <W.robson1@newcastle.ac.uk>
2021-04-29 16:52:24 +0100

    Completed R practical

--- basic_layout.R
+++ basic_layout.R
@@ -1,24 +1,22 @@
 library(shiny)
 
+habitats <- read.csv("www/habitats.csv")
+
 # Define UI ----
 
-ui <- fluidPage(titlePanel("Title panel"),
-                  img(src=wfarm), 
+ui <- fluidPage(titlePanel("Windfarms in Cumbria"),
                   sidebarLayout(
-                  sidebarPanel("MySidebar",
-                  h3("a button"),
+                  sidebarPanel("",
+                  h3("Click here for submission"),
                   actionButton(inputId="my_submitstatus",
-                  label="Submit"), checkboxGroupInput(inputId = "my_checkgroup", 
-                                                     h3("Select one habitat"), 
-                                                     choices = list("Woodland" = 1, 
-                                                                    "Grassland" = 2, 
-                                                                    "Urban" = 3),
-                                                     selected = 1)), position = "left",
-           mainPanel(
-          p("This website will be to help planners assess potential windfarm
+                  label="Submit"), radioButtons(inputId = "my_checkgroup", 
+                  h3("Select one habitat"), 
+                  choices = list("Woodland" = 1,"Grassland" = 2, "Urban" = 3), selected = 1)),
+                  
+           mainPanel( p("This website will be to help planners assess potential windfarm
            development areas in Cumbria, and achieve a ",strong("balance"),
-           "between different ", em("interest groups"), "and other users."
-           )
+           "between different ", em("interest groups"), "and other users.", img(src=wfarm)
+           ), plotOutput(outputId = "habitats_plot")
        )))
 
 
@@ -28,7 +26,9 @@ ui <- fluidPage(titlePanel("Title panel"),
 
 # Define server logic ----
 server <- function(input, output) {
-  
+  output$habitats_plot <- renderPlot(
+    hist(habitats[,as.numeric(input$my_checkgroup)])
+  )
 }
 
 # Run the app ----
